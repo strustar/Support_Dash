4b8589e623aa8e7e3e99e4c90fc4f2384610633f
strustar <123219960+strustar@users.noreply.github.com>
2023-07-16 17:35:44 +0900

    Add files via upload

--- support.py
+++ support.py
@@ -1,9 +1,10 @@
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+# app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+fs1 = 24;  fs2 = 20;  fs3 = 16
 
-# the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
     "position": "fixed",                  # 요소의 위치를 상대적이 아닌 고정된 위치로 설정
     "top": "10px",                       # 상단에서의 위치를 100 픽셀로 설정
@@ -17,15 +18,15 @@ SIDEBAR_STYLE = {
     "border-radius": "0.25rem",           # 테두리 반경 (둥근 모서리)을 0.25rem으로 설정
     # "box-shadow": "0 4px 6px rgba(0, 0, 0, 0.1)",  # 박스 그림자 추가
     "font-family": "'Roboto', sans-serif",         # 글꼴 변경
-    "color": "red",                   # 글자색을 설정
+    # "color": "red",                   # 글자색을 설정
     'font-weight':'bold',
+    'font-size':f'{fs3}px',
     "overflow-y": "auto",  # 스크롤 기능 추가
 }
 
 print(SIDEBAR_STYLE['position'])
 SIDEBAR_STYLE['top'] = '10px'
 
-fs1 = 24;  fs2 = 20;  fs3 = 16
 style = {
     "color": 'black',
     'font-size':f'{fs3}px',
@@ -39,119 +40,94 @@ style_center = {
     "align-items": "center",
     "width": "100%",
 }
+style_number = {
+    "width": "50%",
+    'font-weight': 'bold',
+    'background-color': 'linen',
+    'color': 'black',
+    "border": "1px solid gray",
+    'padding': '5px'
+}
 
 header = html.P("[Information : 입력값]", style={**style, **style_center, **{'color':'blue', 'font-size':f'{fs1}px'}})
+s_h = ['s_h', 2000]
+s_t = ['s_t', 350]
 Slab = html.Div([
     html.P('1. 슬래브', style={**style, **{'font-size':f'{fs2}px'}}),
-    html.P('층고 [mm]', style={**style, **{'font-size':f'{fs3}px'}}),
-    ])
-
-form = dbc.Row(
-    [
-        dbc.Col(
-            [
-                dbc.Label("Email", html_for="example-email-grid"),
-                dbc.Input(
-                    type="number", value = 100,
-                    id="example-email-grid",
-                    placeholder="Enter email",
-                    style={"width": "50%"},
-                ),
-            ],
-            # width=6,
-        ),
-        dbc.Col(
-            [
-                dbc.Label("Password", html_for="example-password-grid"),
-                dbc.Input(
-                    type="number",
-                    id="example-password-grid",
-                    placeholder="Enter password",
-                ),
-            ],
-            # width=6,
-        ),
-    ],
-    className="g-3",
-)
-
-sidebar = dbc.Container(
-    [
-        header,
-        Slab,
-        form,
-        dbc.InputGroup(
-        [
-            html.Div(
-                [
-                    html.Span("문자 입력 2", className="input-group-text")
-                ], className="input-group-prepend"
+    dbc.Row([
+        dbc.Col([
+            dbc.Label("층고 [mm]", html_for=s_h[0], style={'margin-left':'0px'}),
+            dbc.Input(
+                type="number", value=s_h[1], step=10,
+                id=s_h[0],
+                # placeholder="Enter number",
+                style=style_number,
             ),
-            dbc.Input(id="text-input-1", type="text"),
-        ], className="mb-3"
-        ),
-        dbc.InputGroup(
-            [
-                html.Span("문자 입력 2", className="input-group-text d-flex align-items-center"),
-                dbc.Input(id="text-input-2", type="text"),
-            ], className="mb-3"
-        ),
-
-        dbc.InputGroup(
-            [
-                html.Span("숫자 입력 1", className="input-group-text d-flex align-items-center"),
-                dbc.Input(id="number-input-1", type="number"),
-            ], className="mb-3"
-        ),
-
-        dcc.RadioItems(
-            id='radio-items-2',
-            options=[
-                {'label': 'Option 3', 'value': 3},
-                {'label': 'Option 4', 'value': 4}
-            ],
-            value=3,
-            className="mb-3"
-        ),
-    ],
-    # className="my-text",
-    style=SIDEBAR_STYLE,
+        ],),
+        dbc.Col([
+            dbc.Label("두께 [mm]", html_for=s_t[0]),
+            dbc.Input(
+                type="number", value=s_t[1], step=10,
+                id=s_t[0],
+                style=style_number,
+            ),
+        ],),
+    ], style={'margin-left': '10px', 'margin-right': '10px'}),
+], style={"border": "2px solid gray",'padding': '5px'},    
 )
+
+stored_values = html.Div(
+    [
+        html.Div(id="hidden_s_h", children=""),
+        html.Div(id="hidden_s_t", children="")
+    ],
+    style={"display": "none"}
+)
+
+sidebar = dbc.Container( [header, Slab, ], style=SIDEBAR_STYLE,)
+
 
 main_content = dbc.Container(id="main-content")
 
-app.layout = dbc.Container(
+
+app.layout = html.Div(
     [
-        dbc.Row(
+        dbc.Container(
             [
-                dbc.Col(sidebar),
-                dbc.Col(main_content),
-            ], align="top"
+                dbc.Row(
+                    [
+                        dbc.Col(sidebar),
+                        dbc.Col(main_content),
+                    ], align="top"
+                ),
+                dbc.Row(  # 새로운 행을 추가하여 변경 값 출력
+                    [
+                        html.Label("s_h 값: "),
+                        html.Span(id="output-s_h-value"),
+                        html.Label(" s_t 값: "),
+                        html.Span(id="output-s_t-value")
+                    ],
+                    style={'margin-left': '700px', 'font-size': '20px'}
+                )
+            ], fluid=True
         ),
-    ], fluid=True
+        stored_values
+    ]
 )
 
-# @app.callback(
-#     Output("main-content", "children"),
-#     [
-#         Input("text-input-1", "value"),
-#         # Input("text-input-2", "value"),
-#         # Input("number-input-1", "value"),
-#         # Input("number-input-2", "value"),
-#         # Input("radio-items-1", "value"),
-#         # Input("radio-items-2", "value"),
-#     ],
-# )
-# def update_main_content(ti1, ti2, ni1, ni2, ri1, ri2):
-#     return html.Div([
-#         html.H4("사용자 입력:"),
-#     #     html.P(f"문자 입력 1: {ti1}"),
-#     #     html.P(f"문자 입력 2: {ti2}"),
-#     #     html.P(f"숫자 입력 1: {ni1}"),
-#     #     html.P(f"숫자 입력 2: {ni2}"),
-#     #     html.P(f"라디오 버튼 1: {ri1}"),
-#     #     html.P(f"라디오 버튼 2: {ri2}"),
-#     ])
+@app.callback(
+    [Output("hidden_s_h", "children"),
+    Output("hidden_s_t", "children")],
+    [Input(s_h[0], "value"),
+    Input(s_t[0], "value")]
+)
+def update_input_values(s_h_value, s_t_value):
+    # print(f's_h_value: {s_h_value}')  # 콘솔에 입력 값 출력
+    # print(f's_t_value: {s_t_value}')  # 콘솔에 입력 값 출력
+    return s_h_value, s_t_value
+
+s = update_input_values
+print(s)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
