bfb40ee082fe7d05e1c8a5e10caae22b12a77cdb
strustar <123219960+strustar@users.noreply.github.com>
2023-07-21 19:51:26 +0900

    tt

--- side.py
+++ side.py
@@ -1,14 +1,14 @@
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 
-fs = [28, 24, 20, 16]
+fs = [28, 24, 20, 16];  width_side = 500
 
-SIDEBAR_STYLE = {
+style_sidebar = {
     "position": "fixed",                  # 요소의 위치를 상대적이 아닌 고정된 위치로 설정
     "top": "10px",                       # 상단에서의 위치를 100 픽셀로 설정
     "left": "10px",                       # 좌측에서의 위치를 10 픽셀로 설정
     "bottom": "10px",                      # 하단에서의 위치를 0 픽셀로 설정
-    "width": "500px",                     # 너비를 32rem으로 설정
+    "width": f"{width_side}px",                     # 너비를 32rem으로 설정
     "padding": "10px",               #  padding, margin 속성은 top, right, bottom, left 값을 순서
     # "margin": "10px",                    # 바깥쪽 여백을 모든 방향에 100 픽셀로 설정
     "background-color": "#f8f9fa",        # 배경색을 설정
@@ -21,7 +21,6 @@ SIDEBAR_STYLE = {
     'font-size':f'{fs[3]}px',
     "overflow-y": "auto",  # 스크롤 기능 추가
 }
-
 style = {
     "color": 'black',
     'font-size':f'{fs[3]}px',
@@ -46,24 +45,24 @@ style_number = {
 
 class In:
     pass
-In.s_h = ['s_h', 2000];  In.s_t = ['s_t', 350]
+In.s_h = {'id':'s_h', 'v':2000}
+In.s_t = {'id':'s_t', 'v':350}
 
 def Sidebar():
     header = html.P("[Information : 입력값]", style={**style, **style_center, **{'color':'blue', 'font-size':f'{fs[1]}px'}})    
-    Slab = html.Div([html.P('1. 슬래브', style={**style, **{'font-size':f'{fs[2]}px'}}),
+    Slab = html.Div([html.Span('< 슬래브 >', style={**style, **{'font-size':f'{fs[2]}px'}}),
+        html.Hr(),
         dbc.Row([
             dbc.Col([
-                dbc.Label("층고 [mm]", html_for=In.s_h[0], style={'margin-left':'0px'}),
-                dbc.Input(type="number", value=In.s_h[1], step=10, id=In.s_h[0], style=style_number,),
-            ],),
+                dbc.Label("층고 [mm]", html_for=In.s_h['id'], style={'margin-left':'0px'}),
+                dbc.Input(type="number", value=In.s_h['v'], step=10, id=In.s_h['id'], style=style_number,), ],),
             dbc.Col([
-                dbc.Label("두께 [mm]", html_for=In.s_t[0]),
-                dbc.Input(type="number", value=In.s_t[1], step=10, id=In.s_t[0], style=style_number,),
-            ],), # width=3),
+                dbc.Label("두께 [mm]", html_for=In.s_t['id']),
+                dbc.Input(type="number", value=In.s_t['v'], step=10, id=In.s_t['id'], style=style_number,), ],), # width=3),
         ], style={'margin-left': '10px', 'margin-right': '10px'}),
     ], style={"border": "2px solid gray",'padding': '5px'}, )
 
-    sidebar = dbc.Container( [header, Slab, ], style=SIDEBAR_STYLE,)
+    sidebar = dbc.Container( [header, Slab, ], style=style_sidebar,)    
     return sidebar
 
 # def Callback():
--- support.py
+++ support.py
@@ -1,39 +1,86 @@
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
-import json
+import re
 import pandas as pd
-from side import Sidebar, fs, In
+from side import Sidebar, fs, In, style_sidebar, width_side
+
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 # app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
-# s_h = ['s_h', 2000];  s_t = ['s_t', 350]
+style = {} #{"border": "1px solid black", 'width':'100%', 'backgroundColor':'yellow', "textDecoration": "underline", }
+label_style = {
+    "color": 'gray',
+    'font-size':f'{fs[2]}px',
+    "font-weight": "bold",    
+    'textAlign':'center',
+    'margin-top':'0px',
+    'padding-left':'0px',    
+}
+active_label_style={**label_style, **{'color':'blue', 'font-size':f'{fs[1]}px', "backgroundColor": "deepskyblue", "borderRadius": "50px"}}
+
+tab_style={"backgroundColor": "", "border": "3px dashed lightblue", "borderRadius": "50px", 'width':'20%',
+        'marginRight':'50px'}
+
+active_tab_style = {**tab_style, **{"border": "3px dashed blue"}}
+common_style = {
+    "label_style": label_style,
+    "active_label_style": active_label_style,
+    "tab_style": tab_style,
+    "active_tab_style": active_tab_style,    
+    }
+
+tabs = dbc.Tabs(children=[
+        dbc.Tab([html.P('tttt'), html.Div(id='t1')], label="\u2002 Ⅰ. 설계조건 📝", tab_id='tab1', **common_style),                 
+        dbc.Tab(label="\u2002 Ⅱ. 구조   검토 💻", tab_id='tab2', **common_style), 
+        dbc.Tab(label="\u2002 Ⅲ. 요약 ⭕", tab_id='tab3', **common_style), 
+        dbc.Tab(label="\u2002 [참고 사항] ✍️", tab_id='tab4', **common_style), 
+        ], id='tabs', active_tab='tab1',  style=style, )
 
 
+style_main = style_sidebar.copy()
+style_main.update({'left':f'{width_side + 50}px', 'top':'70px','width':'1200px', 'background':'white'})
 main_content = dbc.Container(id="main-content")
+
 app.layout = dbc.Container([
     html.Div([
         dbc.Row([
             dbc.Col(Sidebar()),
-            dbc.Col(main_content),
+            dbc.Col([main_content, tabs], style=style_main),
         ], align="top", ), 
     ],),
 ], fluid=True, )
 
 
 @app.callback(
-    Output("main-content", "children"),
-    [Input(In.s_h[0], "value"), Input(In.s_t[0], "value")],
+    Output("t1", "children"),
+    [Input(In.s_h['id'], "value"), Input(In.s_t['id'], "value")],
 )
 def update_output(h, t):
-    In.s_h[1] = h
-    In.s_t[1] = t
+    In.s_h['v'] = h
+    In.s_t['v'] = t
     print(In.s_h, In.s_t)
+    return html.Div([f"Input 1: {h}, Input 2: {t}"])
     # data = {In.s_h[0]:h, In.s_t[0]:t}    
     # with open("In.json", "w") as f:
     #     json.dump(data, f)
     # return html.Div([]) #, print(height, thickness)
 
+
+# @app.callback(
+#     Output("main-content", "children"),
+#     [Input("tabs", "active_tab")],
+# )
+# def update_output(tab):
+#     print(tab)
+#     if tab == "tab1":
+#         return html.Div(id="t1", children="Tab 1 is selected.")
+#     elif tab == "tab2":
+#         return html.Div("Tab 2 is selected.")
+#     elif tab == "tab3":
+#         return html.Div("Tab 3 is selected.")
+#     else:
+#         return None
 
 # # JSON 파일 읽기
 # with open("data.json", "r") as f:
@@ -52,4 +99,27 @@ def update_output(h, t):
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+    # app.run_server(debug=True, dev_tools_hot_reload=True)
 
+
+# hupper -m support{파일이름}  # 에러가 발생해도 자동 실행 !!! 
+
+
+
+# def Sidebar():
+#     header = html.P("[Information : 입력값]", style={**style, **style_center, **{'color':'blue', 'font-size':f'{fs[1]}px'}})    
+#     Slab = html.Div([html.P('1. 슬래브', style={**style, **{'font-size':f'{fs[2]}px'}}),
+#         dbc.Row([
+#             dbc.Col([
+#                 dbc.Label("층고 [mm]", html_for=In.s_h[0], style={'margin-left':'0px'}),
+#                 dbc.Input(type="number", value=In.s_h[1], step=10, id=In.s_h[0], style=style_number,),
+#             ],),
+#             dbc.Col([
+#                 dbc.Label("두께 [mm]", html_for=In.s_t[0]),
+#                 dbc.Input(type="number", value=In.s_t[1], step=10, id=In.s_t[0], style=style_number,),
+#             ],), # width=3),
+#         ], style={'margin-left': '10px', 'margin-right': '10px'}),
+#     ], style={"border": "2px solid gray",'padding': '5px'}, )
+
+#     sidebar = dbc.Container( [header, Slab, ], style=style_box,)
+#     return sidebar
